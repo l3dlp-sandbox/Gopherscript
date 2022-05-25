849fd079cbb170a365c61b33528a981318bc050d
TheFuckingManual <84961291+TheFuckingManual@users.noreply.github.com>
2022-05-25 17:36:27 +0200

    gos: minor history improvement

--- cmd/gos/gos.go
+++ cmd/gos/gos.go
@@ -389,6 +389,9 @@ func startShell(state *gopherscript.State, ctx *gopherscript.Context, config REP
 
 		case Enter:
 			history.Commands = append(history.Commands, string(input))
+			if history.Commands[0] == "" {
+				history.Commands = history.Commands[1:]
+			}
 
 			inputString := string(input)
 
